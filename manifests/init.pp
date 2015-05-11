05c590cab6b9593cdf2e30657be9d61c1a479278
Michael Ferguson <mpherg@gmail.com>
2015-05-10 21:17:53 -0600

    Fix name of package

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 # Installs MindNode demo into /Applications
 class mindnode {
-  package { 'MindNodeDemo':
+  package { 'MindNode Pro Demo':
     provider => 'compressed_app',
     source   => 'http://mindnode.com/download/MindNodePro-Demo.zip',
   }
--- spec/classes/mindnode_spec.rb
+++ spec/classes/mindnode_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'mindnode' do
   it do
-    should contain_package('MindNodeDemo').with({
+    should contain_package('MindNode Pro Demo').with({
       :provider => 'compressed_app',
       :source   => 'http://mindnode.com/download/MindNodePro-Demo.zip',
     })
